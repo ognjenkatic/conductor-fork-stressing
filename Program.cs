6c67b3f9e37bee4715d00f1fde48819a5c467cd5
ognjenkatic <>
2025-05-05 13:22:48 +0200

    Update CS version

--- Program.cs
+++ Program.cs
@@ -1,16 +1,22 @@
 ﻿using System.Reflection;
 using ConductorJoinIssue;
+using ConductorSharp.Engine;
 using ConductorSharp.Engine.Extensions;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 
 var builder = Host.CreateApplicationBuilder(args);
+
+builder.Logging.AddFilter("System.Net.Http.HttpClient.ConductorSharp.Client", LogLevel.None);
+
 builder
     .Services.AddConductorSharp(baseUrl: "http://localhost:48081")
-    .AddExecutionManager(1000, 250, 100, null, [Assembly.GetExecutingAssembly()]);
+    .AddExecutionManager(1000, 250, 100, null, [Assembly.GetExecutingAssembly()])
+    .UseBetaExecutionManager();
 
 builder.Services.RegisterWorkerTask<ForkyDorkyWorker>();
 builder.Services.RegisterWorkerTask<SleepyDeepyWorker>();
 builder.Services.RegisterWorkflow<ForkyWorkyWorkflow>();
-
 var host = builder.Build();
 host.Run();
